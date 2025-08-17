cb840fdc696ddbfc76191508322e6fefcb0cfb37
MustafaKoceerr <mustafakoceerr@hotmail.com>
2025-08-17 15:45:47 +0300

    Introduce Constants for shared keys and timeouts. Reduce magic values.

---
+++ app/src/main/java/com/example/mustafakocer/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.mustafakocer.util
+
+
+object PagingConstants {
+    const val PRODUCT_PAGE_SIZE = 20
+    const val ORDER_PAGE_SIZE = 10
+}
+
+object FirebaseConstants {
+    const val PATH_CARTS = "carts"
+}
