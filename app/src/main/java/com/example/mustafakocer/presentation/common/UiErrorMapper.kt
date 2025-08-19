f05fc1eb5fc627363be7f0a3b44b2f014e1ee79b
MustafaKoceerr <mustafakoceerr@hotmail.com>
2025-08-19 11:00:28 +0300

    Add UiError and UiErrorMapper to translate domain failures into user-friendly messages.

---
+++ app/src/main/java/com/example/mustafakocer/presentation/common/UiErrorMapper.kt
@@ -0,0 +1,35 @@
+package com.example.mustafakocer.presentation.common
+
+
+import com.example.mustafakocer.R
+import com.example.mustafakocer.domain.exception.AppException
+import javax.inject.Inject
+
+class UiErrorMapper @Inject constructor() {
+
+    fun map(exception: AppException): UiError {
+        return when (exception) {
+            is AppException.Network.NoInternet -> UiError(
+                title = R.string.error_title_no_internet,
+                subtitle = R.string.error_subtitle_no_internet,
+                icon = R.drawable.ic_cloud_off_24
+            )
+            is AppException.Server.Unauthorized, is AppException.Session.MissingSessionData -> UiError(
+                title = R.string.error_title_unauthorized,
+                subtitle = R.string.error_subtitle_unauthorized,
+                icon = R.drawable.ic_lock_24,
+                retryButtonText = R.string.action_login
+            )
+            is AppException.Server -> UiError(
+                title = R.string.error_title_server,
+                subtitle = R.string.error_subtitle_server,
+                icon = R.drawable.ic_server_error_24
+            )
+            else -> UiError(
+                title = R.string.error_title_unknown,
+                subtitle = R.string.error_subtitle_unknown,
+                icon = R.drawable.ic_error_outline_24
+            )
+        }
+    }
+}
