a093d35cb6cb069975bbf033b54d9be04482ad12
MustafaKoceerr <mustafakoceerr@hotmail.com>
2025-08-20 22:52:17 +0300

    Introduce FirebaseConstants; rename Constants to PagingConstants; remove obsolete ViewUtil.

--- app/src/main/java/com/example/mustafakocer/util/PagingConstants.kt
+++ app/src/main/java/com/example/mustafakocer/util/PagingConstants.kt
@@ -1,11 +1,10 @@
 package com.example.mustafakocer.util
 
-
+/**
+ * Defines constants related to the Paging 3 library configuration,
+ * such as page sizes for different data lists.
+ */
 object PagingConstants {
     const val PRODUCT_PAGE_SIZE = 20
     const val ORDER_PAGE_SIZE = 10
-}
-
-object FirebaseConstants {
-    const val PATH_CARTS = "carts"
 }
